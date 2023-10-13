7f7f57e76b5b5aa70ec6d829f3c07ed0a6f1e972
Xin Deng <xind1470@gmail.com>
2023-10-12 19:29:30 -0700

    Final draft of labs to submit

--- code-102/script.js
+++ code-102/script.js
@@ -1,95 +1,84 @@
-function askName(){
+let usersName;
+
+function askName() {
   alert("We have 10 new kittens ready for adoption!");
-  let usersName = prompt("What is your name?");
+  usersName = prompt("What is your name?");
   console.log(usersName);
   return usersName;
 }
 
-function adoptOrNo(){
-  let usersAnswer = prompt("Do you want to adopt our cats? Type Yes or No.");
-  console.log("User typed: " + usersAnswer);
-  return usersAnswer;
-}
+let usersAnswer;
 
-function usersAnswerHeader(){
-  if (usersAnswer.toLowerCase == "Yes") {
+function adoptOrNo() {
+  usersAnswer = prompt(
+    "Do you want to adopt our cats? Type Yes or No."
+  ).toLowerCase();
+  console.log(usersAnswer);
+
+  if (usersAnswer == "yes") {
+    console.log(usersAnswer);
     document.write(
       "Welcome " +
-        userName +
+        usersName +
         "! Then please scroll down to check out some of our longest residents or click Adopt Now!"
     );
-  } else if (usersAnswer.toLowerCase == "No") {
-    usersAnswer.toLowerCase = prompt(
+  } else if (usersAnswer == "no") {
+    console.log(usersAnswer);
+    usersAnswer = prompt(
       "Are you sure? Let's try again. Do you want to adopt our cats? Type Yes or No."
     );
 
-    if (usersAnswer.toLowerCase == "Yes") {
+    if (usersAnswer == "yes") {
       document.write(
         "Welcome " +
-          userName +
+          usersName +
           "! Then please scroll down to check out some of our longest residents or click Adopt Now!"
       );
-
-      if (usersAnswer.toLowerCase == "No") {
-        document.write(
-          "Well " +
-            userName +
-            "... maybe you should google some dog adoption sites..."
-        );
-      } else {
-        document.write(
-          "Well " +
-            userName +
-            "... maybe cats are not suitable for you if you can't follow directions"
-        );
-        return usersAnswerHeader;
-      }
+    } else if (usersAnswer == "no") {
+      document.write(
+        "Well " +
+          usersName +
+          "... maybe you should google some dog adoption sites..."
+      );
+    } else {
+      document.write(
+        "Well " +
+          usersName +
+          "... maybe cats are not suitable for you if you can't follow directions"
+      );
+      return usersAnswer;
     }
   }
 }
 
-function belowHeader(){
-  if (usersAnswerHeader == 'Yes') {
-      document.write("Glad you're here!")
-  if (usersAnswerHeader == 'No')
-        document.write("You're welcome to stay I guess...")
+function belowHeader() {
+  if (usersAnswer == "yes") {
+    document.write("Glad you're here!");
+  } else if (usersAnswer == "no") {
+    document.write("You're welcome to stay I guess...");
+  }
 }
 
-function footer (){
-  if (usersAnswerHeader == 'Yes') {
-      document.write("Go meet Truck!")
-  if (usersAnswerHeader == 'No')
-        document.write("Go still meet Truck anyways")
+function footer() {
+  if (usersAnswer == "yes") {
+    document.write("Go meet Truck!");
+  } else if (usersAnswer == "no") {
+    document.write("Go meet Truck anyways");
+  }
 }
 
-// if (usersAnswer.toLowerCase == 'Yes') {
-//   document.write("Glad you're here!")
-// }
+function guessCatNumber() {
+  let userGuess = prompt("Guess how many cats are on this page!");
+  while (userGuess != 4) {
+    userGuess = prompt("Try again! Hint: there's less than 5");
+  }
+  alert("You are correct!");
+}
 
-// if (usersAnswer == 'Yes') {
-//   document.write("Go meet Truck")
-// }
 
-// function adoptOrNo(){
-//   let usersAnswer = prompt(
-//     "Do you want to adopt our cats? Type Yes or No."
-//   );
-//   console.log("User typed: " + usersAnswer);
-//   return usersAnswer;
-
-//   function usersAnswer (){
-//   if (usersAnswer.toLowerCase == 'Yes') {
-//     document.write("Welcome " + userName +
-//       "! Then please scroll down to check out some of our longest residents or click Adopt Now!"
-//   );
-// } else if (usersAnswer.toLowerCase == "No") {
-//   document.write(
-//     "Well " +
-//       userName +
-//       "... maybe you should google some dog adoption sites..."
-//   );
-// } else {
-//   document.write(
-//     "Well " +
-//       userName +
-//       "... maybe cats are not suitable for you if you can't follow directions"
+function giveUserACat (){
+  let theCatAmount = prompt('How many cats do you want?')
+  for (let i = 1; i <= theCatAmount; i++ ) {
+    document.write ('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStpFeLQOVcNN-ISh8J7xFomAtEwAYss1_jBQ&usqp=CAU" alt="simple small cat icon">');
+  }
+}
